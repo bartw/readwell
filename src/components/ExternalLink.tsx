e3e9ff42726d9ba157351f04d3cbaa0497777f86
Bart Wijnants <bartwijnants@outlook.com>
2018-08-17 19:45:51 +0200

    styled components

--- src/components/ExternalLink.tsx
+++ src/components/ExternalLink.tsx
@@ -1,18 +1,24 @@
 import * as React from "react";
+import styled from "../styled";
 
 export declare interface ExternalLinkProps {
+  className?: string;
   children: React.ReactNode;
   href: string;
 }
 
-export default class ExternalLink extends React.PureComponent<
-  ExternalLinkProps
-> {
+class ExternalLink extends React.PureComponent<ExternalLinkProps> {
   render(): React.ReactNode {
     return (
-      <a href={this.props.href} target="_blank" rel="noopener noreferrer">
+      <a className={this.props.className} href={this.props.href} target="_blank" rel="noopener noreferrer">
         {this.props.children}
       </a>
     );
   }
 }
+
+const StyledExternalLink = styled(ExternalLink)`
+  text-decoration: none;
+`;
+
+export default StyledExternalLink;
